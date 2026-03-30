7ce62a0bef35828a2367e7bf9f43252219dce497
CarlosARetuertoD <cretuertodelgado@gmail.com>
2026-03-29 21:36:14 -0500

    fix: move supabase key to env vars

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,8 +1,8 @@
 import { createClient } from '@supabase/supabase-js';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
-const SUPABASE_URL = 'https://oihcnwjaqmygctmahwlp.supabase.co';
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9paGNud2phcW15Z2N0bWFod2xwIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc3MzUxNDE3MywiZXhwIjoyMDg5MDkwMTczfQ.NaJYnCGwEPT1MTrjxdQYqdk3cSB1uFnUflhJkVBylaM';
+const SUPABASE_URL = process.env.EXPO_PUBLIC_SUPABASE_URL!;
+const SUPABASE_ANON_KEY = process.env.EXPO_PUBLIC_SUPABASE_KEY!;
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
   auth: {
